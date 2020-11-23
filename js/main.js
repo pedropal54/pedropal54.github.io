25e2406f24a89b3762ae6104a311295cb96532ed
pedropal54 <58046380+pedropal54@users.noreply.github.com>
2020-11-23 15:46:17 +0100

    v1.1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let ubicacionPrincipal = window.pageYOffset; //0
 
   AOS.init();
 
+/*
 window.addEventListener("scroll", function(){
     let desplazamientoActual = window.pageYOffset; //180
     if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
@@ -11,7 +12,7 @@ window.addEventListener("scroll", function(){
     }
     ubicacionPrincipal= desplazamientoActual; //200
 
-})
+})*/
 
 // Menu
 
